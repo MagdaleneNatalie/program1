975dc961fd24982b45af02526afc163579b5ddff
Przemek <przemekworld@gmail.com>
2017-01-13 22:37:09 +0100

    Dodanie projektu testowego XUnit i XUnit Visual Studio

--- TicTacToeTests/TicTacToeTests.cs
+++ TicTacToeTests/TicTacToeTests.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace TicTacToeTests
 {
-    public class Class1
+    public class TicTacToeTests
     {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(0, 1);
+        }
     }
 }
