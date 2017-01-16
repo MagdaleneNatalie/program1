ae5e431a94a5a29f253193e9460749669fea00e5
Przemek <przemekworld@gmail.com>
2017-01-16 19:24:31 +0100

    Szablon klienta TCP/IP

--- TicTacToeClient/Program.cs
+++ TicTacToeClient/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,42 @@ namespace TicTacToeClient
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Int32 port = 13000;
+                TcpClient client = new TcpClient("127.0.0.1", port);
+
+                const string message = "Bla bla bla";
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+
+                NetworkStream stream = client.GetStream();
+
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", message);
+
+
+                data = new Byte[256];
+
+                String responseData = String.Empty;
+
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
         }
     }
 }
